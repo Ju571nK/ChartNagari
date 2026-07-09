9037bc18fafab7e7754bb02dd7c8edad1d59ac4b
Justin Kwon <yskwon0830@gmail.com>
2026-07-09 20:11:47 +0900

    v2.11.0.0 feat: zero-install GitHub Pages demo + bundle optimization (#46)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { DEMO_STATIC, installDemoApi } from './demoApi'
 import './i18n'
 import './App.css'
+
+// Zero-install demo build: serve all /api calls from canned fixtures.
+if (DEMO_STATIC) {
+  installDemoApi()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
